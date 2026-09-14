d8283116a1a6f7b69b3aaffc4b3650b1649ca820
Alan <alangyraldo3@gmail.com>
2026-09-14 09:24:26 -0500

    Agrega ejercicio5 - generador de nombre de usuario

---
+++ variables-tipos-de-datos/ejercicio5.js
@@ -0,0 +1,9 @@
+let PrimerNombre = "Carlos";
+let PrimerApellido = "Gomez";
+let añodenacimiento = 1998;
+
+console.log(PrimerNombre.toLowerCase());
+console.log(PrimerApellido.toLowerCase());
+console.log(String(añodenacimiento).slice(-2));
+
+console.log(PrimerNombre[0].toLowerCase() + PrimerApellido.toLowerCase());
